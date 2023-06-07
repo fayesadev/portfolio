54ad695318d0136c2f14474b488dd3456f6de93c
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-06-07 12:39:08 -0600

    Add profile pic

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,21 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hey, I'm Faye and I'm A Web Developer</h1>
-      <h3>Building applications that enrich user experience.</h3>
-      <button class="border-2">
-        Let's connect
-      </button>
+      <div>
+        <h1>Hey, I'm Faye and I'm A Web Developer</h1>
+        <h3>Building applications that enrich user experience.</h3>
+        <button class="border-2">Let's connect</button>
+      </div>
+      <div>
+        <Image 
+          src={"/Final.jpg"}
+          width={200}
+          height={200}
+          alt="Picture of Faye" 
+        />
+      </div>
     </main>
-  )
+  );
 }
