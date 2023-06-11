3e10e88f01fb6d11167217e46af4ae5683ae8214
Faye Dumbrigue <fayedumbrigue@gmail.com>
2023-06-10 22:40:08 -0600

    Create nav html skeleton

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -7,7 +7,7 @@ import NavItem from "./NavItem";
 const MENU_LIST = [
   { text: "Home", href: "/" },
   { text: "About Us", href: "/about" },
-  { text: "Contact", href: "/contact"},
+  { text: "Contact", href: "/contact" },
 ];
 
 const Navbar = () => {
@@ -15,6 +15,22 @@ const Navbar = () => {
   const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
-    
-  )
-}
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <a>
+            <h1 className="logo">FayeTheDev</h1>
+          </a>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </nav>
+    </header>
+  );
+};
